22539906bc78be49ca315c69900c64ced081a58f
Xero kun <elo48858@gmail.com>
2023-02-19 15:02:38 +0100

    `✨UPDATE` Created Note Data Class

---
+++ app/src/main/java/dev/xero/cleanarchitecturenotesapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,25 @@
+package dev.xero.cleanarchitecturenotesapp.feature_note.domain.model
+
+import androidx.compose.ui.graphics.Color
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import dev.xero.cleanarchitecturenotesapp.ui.theme.*
+
+@Entity
+data class Note(
+	val title: String,
+	val content: String,
+	val timeStamp: String,
+	val color: Int,
+	@PrimaryKey val id: Int? = null
+) {
+	companion object {
+		val noteColors = listOf<Color> (
+			RedOrange,
+			LightGreen,
+			Violet,
+			SkyBlue,
+			RedPink
+		)
+	}
+}
