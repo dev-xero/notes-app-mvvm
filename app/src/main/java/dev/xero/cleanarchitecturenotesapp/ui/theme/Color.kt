843401b7ef50d2dab7339f8a020483a8086fed23
Xero kun <elo48858@gmail.com>
2023-02-19 14:23:58 +0100

    `🌱INIT` Setup Project

---
+++ app/src/main/java/dev/xero/cleanarchitecturenotesapp/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 - Xero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.xero.cleanarchitecturenotesapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFFFFFFFF)
+
+val DarkGrey = Color(0xFF0F1216)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xFFFDBBA6)
+val RedPink = Color(0xFFFFB4CD)
+val SkyBlue = Color(0xFFA7E3EB)
+val Violet = Color(0xFFD6A7DF)
+val LightGreen = Color(0xFFE5E9B2)
---
+++ app/src/main/java/dev/xero/cleanarchitecturenotesapp/ui/theme/Theme.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 - Xero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.xero.cleanarchitecturenotesapp.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+
+private val ColorPalette = darkColors(
+	primary = White,
+	background = DarkGrey,
+	onBackground = White,
+	surface = LightBlue,
+	onSurface = DarkGrey
+)
+
+@Composable
+fun CleanArchitectureNotesAppTheme(
+	content: @Composable () -> Unit
+) {
+
+	MaterialTheme(
+		colors = ColorPalette,
+		typography = Typography,
+		shapes = Shapes,
+		content = content
+	)
+}
