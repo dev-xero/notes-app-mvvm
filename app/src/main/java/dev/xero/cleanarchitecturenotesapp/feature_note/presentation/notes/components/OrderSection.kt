a642915f95ad2c2331866a592d1b4127af9f6c74
Xero kun <elo48858@gmail.com>
2023-02-19 19:29:37 +0100

    `✨UPDATE` Setup Order Section First Row

---
+++ app/src/main/java/dev/xero/cleanarchitecturenotesapp/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,54 @@
+package dev.xero.cleanarchitecturenotesapp.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import dev.xero.cleanarchitecturenotesapp.R
+import dev.xero.cleanarchitecturenotesapp.feature_note.domain.util.NoteOrder
+import dev.xero.cleanarchitecturenotesapp.feature_note.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+	modifier: Modifier = Modifier,
+	noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+	onOrderChange: (NoteOrder) -> Unit
+) {
+	Column(
+		modifier = modifier
+	) {
+		Row(
+			modifier = Modifier.fillMaxWidth()
+		) {
+			// TITLE ORDER BUTTON
+			DefaultRadioButton(
+				text = stringResource(R.string.order_title),
+				selected = noteOrder is NoteOrder.Title,
+				onSelect = {
+					onOrderChange(NoteOrder.Title(noteOrder.orderType))
+				}
+			)
+			Spacer(modifier = Modifier.width(8.dp))
+
+			// DATE ORDER BUTTON
+			DefaultRadioButton(
+				text = stringResource(R.string.order_date),
+				selected = noteOrder is NoteOrder.Title,
+				onSelect = {
+					onOrderChange(NoteOrder.Date(noteOrder.orderType))
+				}
+			)
+			Spacer(modifier = Modifier.width(8.dp))
+
+			// COLOR ORDER BUTTON
+			DefaultRadioButton(
+				text = stringResource(R.string.order_color),
+				selected = noteOrder is NoteOrder.Title,
+				onSelect = {
+					onOrderChange(NoteOrder.Color(noteOrder.orderType))
+				}
+			)
+		}
+	}
+}
