aaaee80fee50cdd253ea84b3ae741d5553e5ab4d
Xero kun <elo48858@gmail.com>
2023-02-19 15:10:09 +0100

    `✨UPDATE` Created Note Database

---
+++ app/src/main/java/dev/xero/cleanarchitecturenotesapp/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,15 @@
+package dev.xero.cleanarchitecturenotesapp.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import dev.xero.cleanarchitecturenotesapp.feature_note.domain.model.Note
+
+@Database(
+	entities = [Note::class],
+	version = 1
+)
+
+abstract class NoteDatabase: RoomDatabase() {
+
+	abstract val noteDao: NoteDao
+}
