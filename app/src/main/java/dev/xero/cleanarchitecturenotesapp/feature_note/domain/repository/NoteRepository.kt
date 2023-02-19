bbc2970c3ac049bc068c9e38f5061570f3b9992c
Xero kun <elo48858@gmail.com>
2023-02-19 15:15:08 +0100

    `✨UPDATE` Created Note Repository Interface

---
+++ app/src/main/java/dev/xero/cleanarchitecturenotesapp/feature_note/domain/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package dev.xero.cleanarchitecturenotesapp.feature_note.domain.repository
+
+import dev.xero.cleanarchitecturenotesapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+
+	fun getNotes(): Flow<List<Note>>
+
+	suspend fun getNoteById(id: Int): Note?
+
+	suspend fun insertNote(note: Note)
+
+	suspend fun deleteNote(note: Note)
+}
