00e42f82c877c467cdc2c41b4f62349e5c7a88a8
Xero kun <elo48858@gmail.com>
2023-02-19 20:27:05 +0100

    `✨UPDATE` Created Note Item Composable

--- app/src/main/java/dev/xero/cleanarchitecturenotesapp/feature_note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/dev/xero/cleanarchitecturenotesapp/feature_note/presentation/notes/components/NoteItem.kt
@@ -1,8 +1,15 @@
 package dev.xero.cleanarchitecturenotesapp.feature_note.presentation.notes.components
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -10,6 +17,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.ColorUtils
@@ -52,6 +60,41 @@ fun NoteItem(
 					cornerRadius = CornerRadius(cornerRadius.toPx())
 				)
 			}
+		}
+
+		Column(
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(16.dp)
+				.padding(end = 32.dp)
+		) {
+			Text(
+				text = note.title,
+				style = MaterialTheme.typography.h6,
+				color = MaterialTheme.colors.onSurface,
+				overflow = TextOverflow.Ellipsis,
+				maxLines = 1
+			)
+
+			Spacer(modifier = Modifier.height(8.dp))
+
+			Text(
+				text = note.content,
+				style = MaterialTheme.typography.body1,
+				color = MaterialTheme.colors.onSurface,
+				overflow = TextOverflow.Ellipsis,
+				maxLines = 10
+			)
+		}
+
+		IconButton(
+			modifier = Modifier.align(Alignment.BottomEnd),
+			onClick = onDeleteClick
+		) {
+			Icon(
+				imageVector = Icons.Default.Delete,
+				contentDescription = null
+			)
 		}
 	}
 }
